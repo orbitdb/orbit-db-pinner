ffe75a3eaf3a40012a11c73e8abf5e90e42e2480
Jonathan El-Bizri <github@elbizri.com>
2019-06-02 13:18:55 -0700

    Use config file

--- src/ipfsInstance.js
+++ src/ipfsInstance.js
@@ -1,22 +1,7 @@
 const IPFS = require('ipfs')
+const config = require('config');
 
-const ipfs = new IPFS({
-  repo: './orbitdb/pinner',
-  start: true,
-  EXPERIMENTAL: {
-    pubsub: true,
-  },
-  config: {
-    Addresses: {
-      Swarm: [
-        // Use IPFS dev signal server
-        // '/dns4/star-signal.cloud.ipfs.team/wss/p2p-webrtc-star',
-        '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star',
-        // Use local signal server
-        // '/ip4/0.0.0.0/tcp/9090/wss/p2p-webrtc-star',
-      ]
-    },
-  }
-})
+const ipfsConfig = config.get('ipfsConfig')
+const ipfs = new IPFS(ipfsConfig)
 
 module.exports = ipfs
