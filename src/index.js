5123e6af45b1b5c49f76edf154af607c62fb3753
Jonathan El-Bizri <github@elbizri.com>
2019-06-05 13:21:56 -0700

    Update: command-line initialization.

--- src/index.js
+++ src/index.js
@@ -2,35 +2,31 @@ const commandLineArgs = require('command-line-args')
 
 const OrbitPinner = require('./OrbitPinner.js')
 const httpServer = require('./httpServer')
+const manifest = require('./Manifest')
 
 const optionDefinitions = [
   { name: 'address', alias: 'd', type: String },
   { name: 'http', alias: 's', type: Boolean },
-  { name: 'port', alias: 'p', type: Number },
-  { name: 'help', alias: 'h', type: Boolean}
+  { name: 'port', alias: 'p', type: Number }
 ]
+
 const options = commandLineArgs(optionDefinitions)
 
 const {
-  help,
-  address,
-  replicate,
-  http,
-  port
+  address
+, port
 } = options
 
-if (help) {
-  console.log(`help`)
-  process.exit()
-}
-else if (!address && !http && !help) {
+const http = options.httpPort || config.get('http.enabled')
+
+if ( !address && !http ) {
   console.log('Orbit pinner requires an orbitdb address or http to be enabled')
   process.exit()
 }
-else if (address) {
+else if ( address ) {
   new OrbitPinner(address)
 }
-else if (http) {
+else if ( http ) {
   new httpServer(port)
 }
 
