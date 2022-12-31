d597912a0952db3536a0d9f99c7cc6618770c663
Sree Grandhe <sgrandhe@utexas.edu>
2022-12-30 19:48:38 -0600

    Remove dist from git tracking

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-exports.__esModule = true;
-var httpServer_1 = __importDefault(require("./lib/httpServer"));
-var PORT = process.env.PORT || 8000;
-httpServer_1["default"].listen(PORT, function () { return console.log("Orbit-pinner listening on port ".concat(PORT)); });
--- dist/lib/ipfsInstance.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var IPFS = require('ipfs-core');
-var config = require('../config');
-exports["default"] = IPFS.create(config);
