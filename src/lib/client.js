4967f13f23dd1db2b521f1e0f7f2ec68aa63092d
Hayden Young <haydenyoung@wijiti.com>
2024-04-17 21:07:21 +0100

    refactor: Create a pinning client.

---
+++ src/lib/client.js
@@ -0,0 +1,63 @@
+import { pipe } from 'it-pipe'
+import { Requests, Responses, createRequestMessage, parseMessage } from './messages/index.js'
+import { pinnerProtocol } from './protocol.js'
+
+export default async ({ orbitdb, pinnerAddressOrId }) => {
+  const pin = async (dbs) => {
+    let pinned = false
+    const pinDBs = source => {
+      return (async function * () {
+        const addresses = dbs.map(p => p.address)
+        const message = await createRequestMessage(Requests.PIN, addresses, orbitdb.identity)
+        yield message
+      })()
+    }
+
+    const stream = await orbitdb.ipfs.libp2p.dialProtocol(pinnerAddressOrId, pinnerProtocol)
+
+    await pipe(pinDBs, stream, async (source) => {
+      for await (const chunk of source) {
+        const message = parseMessage(chunk.subarray())
+        
+        if (message.type === Responses.OK) {
+          pinned = true
+        }
+      }
+    })
+    
+    return pinned
+  }
+  
+  const unpin = async (dbs) => {
+    let unpinned = false
+    
+    const unpinDBs = source => {
+      return (async function * () {
+        const addresses = dbs.map(p => p.address)
+        const message = await createRequestMessage(Requests.UNPIN, addresses, orbitdb.identity)
+        yield message
+      })()
+    }
+
+    const stream = await orbitdb.ipfs.libp2p.dialProtocol(pinnerAddressOrId, pinnerProtocol)
+
+    await pipe(unpinDBs, stream, async source => {
+      for await (const chunk of source) {
+        const message = parseMessage(chunk.subarray())
+          
+        if (message.type === Responses.OK) {
+          unpinned = true
+        }
+      }
+    })
+    
+    return unpinned
+  }
+
+  return {
+    orbitdb,
+    pin,
+    unpin
+  }
+}
+
--- test/utils/create-pins.js
+++ test/utils/create-pins.js
@@ -3,27 +3,14 @@ import drain from 'it-drain'
 import { Requests, createRequestMessage } from '../../src/lib/messages/index.js'
 import { pinnerProtocol } from '../../src/lib/protocol.js'
 
-export const createPins = async (howMany, client, pinner, sink) => {
-  const defaultSink = async (source) => { await drain(source) }
-  sink = sink || defaultSink
-
+export const createPins = async (howMany, client) => {
   const dbs = []
 
   for (let i = 1; i <= howMany; i++) {
-    dbs.push(await client.open(`db${i}`))
+    dbs.push(await client.orbitdb.open(`db${i}`))
   }
 
-  const pinDBs = source => {
-    return (async function * () {
-      const addresses = dbs.map(p => p.address)
-      const message = await createRequestMessage(Requests.PIN, addresses, client.identity)
-      yield message
-    })()
-  }
+  const pinned = await client.pin(dbs)
 
-  const stream = await client.ipfs.libp2p.dialProtocol(pinner.orbitdb.ipfs.libp2p.peerId, pinnerProtocol)
-
-  await pipe(pinDBs, stream, sink)
-
-  return dbs
+  return { pinned, dbs }
 }
