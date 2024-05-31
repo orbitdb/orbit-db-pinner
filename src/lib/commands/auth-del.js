f9d8260bccc752ca5cb5ad4168bc9f79e2777bd7
Hayden Young <haydenyoung@wijiti.com>
2024-05-31 20:58:50 +0100

    fix: Linting.

--- src/lib/commands/auth-del.js
+++ src/lib/commands/auth-del.js
@@ -4,4 +4,3 @@ import { ControllerRequests } from '../messages/index.js'
 export default async (argv) => {
   await runControllerCommand(ControllerRequests.AUTH_DEL, [argv.id], { directory: argv.directory })
 }
-
--- src/lib/commands/auth-list.js
+++ src/lib/commands/auth-list.js
@@ -3,6 +3,6 @@ import { ControllerRequests } from '../messages/index.js'
 
 export default async (argv) => {
   const res = await runControllerCommand(ControllerRequests.AUTH_LIST, [], { directory: argv.directory })
-  
+
   console.log(res.message)
 }
