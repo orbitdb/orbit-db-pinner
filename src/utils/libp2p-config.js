a57d75a646aa9c1d707e119eebb88e4534c46fcc
haad <haad@headbanggames.com>
2024-08-01 08:40:09 +0300

    Enable tcp and mdns for Orbiter daemon

--- src/utils/libp2p-config.js
+++ src/utils/libp2p-config.js
@@ -1,17 +1,21 @@
 import { identify } from '@libp2p/identify'
 import { noise } from '@chainsafe/libp2p-noise'
 import { yamux } from '@chainsafe/libp2p-yamux'
+import { tcp } from '@libp2p/tcp'
 import { webSockets } from '@libp2p/websockets'
 import { gossipsub } from '@chainsafe/libp2p-gossipsub'
+import { mdns } from '@libp2p/mdns'
 
 export const config = ({ peerId, port } = {}) => {
   const conf = {
     addresses: {
       listen: [
+        '/ip4/0.0.0.0/tcp/0',
       `/ip4/0.0.0.0/tcp/${port || 0}/ws`
       ]
     },
     transports: [
+      tcp(),
       webSockets()
     ],
     connectionEncryption: [
@@ -19,6 +23,9 @@ export const config = ({ peerId, port } = {}) => {
     ],
     streamMuxers: [
       yamux()
+    ],
+    peerDiscovery: [
+      mdns()
     ],
     services: {
       identify: identify(),
